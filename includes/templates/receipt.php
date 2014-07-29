7d7d555c568f33c718881ba189fef5143f982603
Ghost1227 <dgriffiths@ghost1227.com>
2014-07-29 17:19:33 -0500

    Cleanup upload templates

--- includes/templates/receipt.php
+++ includes/templates/receipt.php
@@ -1,50 +1,58 @@
 <?php
+/**
+ * Receipt template
+ *
+ * @package     EDD\UploadFile\Templates\Receipt
+ * @since       1.0.1
+ */
 
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
-function edd_fu_receipt_upload_field( $payment, $edd_receipt_args ) {
+// Exit if accessed directly
+if( !defined( 'ABSPATH' ) ) exit;
 
-	// Get cart contents
-	$cart_items = edd_get_payment_meta_cart_details( $payment->ID, true );
 
-	// Set default file upload
-	$fu_enabled = false;
+/**
+ * Add upload field to receipt
+ *
+ * @since		1.0.1
+ * @param		object $payment The purchase we are working with
+ * @param		array $edd_receipt_args Arguemnts for this receipt
+ * @return		void
+ */
+function edd_upload_file_receipt_upload_field( $payment, $edd_receipt_args ) {
+	$cart_items		= edd_get_payment_meta_cart_details( $payment->ID, true );
+	$upload_enabled	= false;
 
-	// Loop & check
-	if ( count( $cart_items ) > 0 ) {
-		foreach ( $cart_items as $cart_item ) {
-			if ( ( get_post_meta( $cart_item['id'], '_edd_fu_enabled', true ) ? true : false ) ) {
-				$fu_enabled = true;
+	// Check files for upload permission
+	if( count( $cart_items ) > 0 ) {
+		foreach( $cart_items as $cart_item ) {
+			if( get_post_meta( $cart_item['id'], '_edd_upload_file_enabled', true ) ? true : false ) {
+				$upload_enabled = true;
 				break;
 			}
 		}
 	}
 
-	// Check if there is at least one product that has file uploads enabled
-	if ( false === $fu_enabled ) {
+	// Return if we can't upload
+	if( false === $upload_enabled ) {
 		return;
 	}
 
-	// Get EDD options
-	$options = EDD_Upload_File::get_options();
+	// Get button style from EDD core
+	$button_style	= edd_get_option( 'button_style', 'button' );
+	$color			= edd_get_option( 'checkout_color', 'blue' );
 
-	// Get correct button classes
-	$button_style = isset( $options[ 'button_style' ] ) ? $options[ 'button_style' ] : 'button' ;
-	$color				= isset( $options[ 'checkout_color' ] ) ? $options[ 'checkout_color' ] : 'blue' ;
-
-?>
-	<h3><?php _e( 'Upload new file', 'edd-fu' ); ?></h3>
+	?>
+	<h3><?php _e( 'Upload new file', 'edd-upload-file' ); ?></h3>
 	<form action="" method="post" enctype="multipart/form-data">
-		<input type="file" name="edd-fu-file" value="" />
-		<input type="submit" name="Submit" value="<?php _e( 'Upload', 'edd-fu' ); ?>" class="<?php echo $button_style . ' ' . $color; ?>" />
+		<input type="file" name="edd-upload-file" value="" />
+		<input type="submit" name="Submit" value="<?php _e( 'Upload', 'edd-upload-file' ); ?>" class="<?php echo $button_style . ' ' . $color; ?>" />
 	</form>
-
-<?php
-
+	<?php
 }
 
-$edd_fu_options = EDD_Upload_File::get_options();
 
-if ( $edd_fu_options['fu_upload_location'] == 'receipt' ) {
-	add_action( 'edd_payment_receipt_after_table', 'edd_fu_receipt_upload_field', 12, 2 );
+// Hook to the receipt page
+if( edd_get_option( 'edd_upload_file_location' ) == 'receipt' ) {
+	add_action( 'edd_payment_receipt_after_table', 'edd_upload_file_receipt_upload_field', 12, 2 );
 }
