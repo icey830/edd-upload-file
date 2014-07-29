c156aa2b6a2f7ab06605ae78eab449a49869fe4e
Ghost1227 <dgriffiths@ghost1227.com>
2014-07-29 17:24:03 -0500

    Bring everything up to date

--- includes/functions.php
+++ includes/functions.php
@@ -12,6 +12,32 @@ if( !defined( 'ABSPATH' ) ) exit;
 
 
 /**
+ * Return file upload directory
+ *
+ * @since		1.0.1
+ * @return		string $upload_dir The file upload directory
+ */
+function edd_upload_file_get_upload_dir() {
+	$upload_dir = wp_upload_dir();
+
+	return $upload_dir['basedir'] . '/edd-upload-files';
+}
+
+
+/**
+ * Return file upload URL
+ *
+ * @since		1.0.1
+ * @return		string $upload_url The file upload URL
+ */
+function edd_upload_file_get_upload_url() {
+	$upload_dir = wp_upload_dir();
+
+	return $upload_dir['baseurl'] . '/edd-upload-files';
+}
+
+
+/**
  * Process and display error messages
  *
  * @since		1.0.1
@@ -19,18 +45,18 @@ if( !defined( 'ABSPATH' ) ) exit;
  * @return		void
  */
 function edd_upload_file_error( $message ) {
-	if( edd_get_option( 'edd_file_upload_location', 'checkout' ) ) {
+	if( edd_get_option( 'edd_upload_file_location', 'checkout' ) ) {
 		$messages = EDD()->session->get( 'edd_cart_messages' );
 
 		if( ! $messages ) {
 			$messages = array();
 		}
 
-		$messages['edd_file_upload_error'] = $message;
+		$messages['edd_upload_file_error'] = $message;
 
 		EDD()->session->set( 'edd_cart_messages', $messages );
 	} else {
-		echo '<tr><td colspan="2" style="color: #ff0000; font-weight: bold;">{' . $message . '}</td></tr>' . "\n";
+		echo '<tr><td colspan="2" style="color: #ff0000; font-weight: bold;">' . $message . '</td></tr>' . "\n";
 	}
 }
 
@@ -53,14 +79,16 @@ function edd_upload_file_view_files( $payment_id ) {
 
 			if( $uploaded_files != '' && count( $uploaded_files ) > 0 ) {
 				$i = 0;
+				$upload_dir = wp_upload_dir();
+				$upload_dir = $upload_dir['basedir'] . '/edd-upload-files';
 
 				echo '<table class="wp-list-table widefat fixed" cellspacing="0">';
 				echo '<tbody id="edd-upload-files-list">';
 
 				foreach( $uploaded_files as $key => $file ) {
 					echo '<tr class="'  . ( $i % 2 == 0 ? 'alternate' : '' ) . '">';
-					echo '<td class="name column-name">' . EDD_Upload_File_Manager::instance()->get_user_friendly_filename( $file ) . '</td>';
-					echo '<td class="price column-price"><a href="' . EDD_Upload_File_Manager::instance()->get_url() . '/' . $file . '" target="_blank">' . __( 'View File', 'edd-upload-file' ) . '</a></td>';
+					echo '<td class="name column-name">' . EDD_Upload_File_Manager::instance()->get_original_filename( $file ) . '</td>';
+					echo '<td class="price column-price"><a href="' . edd_upload_file_get_upload_url() . '/' . $file . '" target="_blank">' . __( 'View File', 'edd-upload-file' ) . '</a></td>';
 					echo '</tr>';
 
 					$i++;
@@ -69,7 +97,7 @@ function edd_upload_file_view_files( $payment_id ) {
 				echo '</tbody>';
 				echo '</table>';
 			} else {
-				echo '<tr><td colspan="2">' . __( 'No files uploaded', 'edd-upload-file' ) . '</td>';
+				echo __( 'No files uploaded', 'edd-upload-file' );
 			}
 			?>
 		</div>
